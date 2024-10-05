c9a14e6ad6cb31a9654e8940f7e7318c0106a2be
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-10-05 15:24:21 -0400

    Added placeholder display functions

--- robot/run.py
+++ robot/run.py
@@ -13,21 +13,15 @@ from pathlib import Path
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+
 def launch_bot_and_ui():
     bot = StringBot()
     ui = UI(bot)
     command = 0
     while command != -1:
-        CTX['programs'].clear()
-        CTX['programs'].extend(os.listdir(Path("./stringart_files").absolute()))
-        ui.update_programs()
-
         ui.interpret_controls(command=command)
-        print("\n"*4)
 
-        lines = ui.get_lines()
-
-        print(*lines, sep='\n')
+        ui.display()
 
         command = int(input("Command: "))
 
