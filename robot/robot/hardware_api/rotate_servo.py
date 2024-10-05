8fdeb80526707e2d7689190a5513a48c69c53992
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-10-05 15:33:46 -0400

    Added time to travel for arm

--- robot/robot/hardware_api/rotate_servo.py
+++ robot/robot/hardware_api/rotate_servo.py
@@ -1,17 +1,24 @@
 import logging
+import time
 
-from .config import ARM_PIN
+from .config import ARM_PIN, ARM_TIME_PER_60_DEG, ARM_TIME_PER_60_DEG_SAFETY
 
 logger = logging.getLogger("robot.hardware_api.rotate_servo")
 
 servo = ARM_PIN
+TIME_PER_DEG = (ARM_TIME_PER_60_DEG + ARM_TIME_PER_60_DEG_SAFETY) / 60
 
 
 def rotate_arm_to(degrees: float):
     if not hasattr(rotate_arm_to, "arm_angle"):
         setattr(rotate_arm_to, "arm_angle", degrees)
+        old = 0.0
     else:
+        old = rotate_arm_to.arm_angle
         rotate_arm_to.arm_angle = degrees
 
     # TODO: Implement fully.
     logger.info(f"Rotating arm to {degrees:.3f} degrees")
+
+    deg_difference = abs(old-degrees)
+    time.sleep(deg_difference * TIME_PER_DEG)
